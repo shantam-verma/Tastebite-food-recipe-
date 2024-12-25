93b8e2df816d3ced02caa9a36c544cdf235df7fe
Shantam Verma <contactshantam@gmail.com>
2024-12-25 20:58:40 +0530

    login page routing bug in production

--- app/components/forms/LoginForm.js
+++ app/components/forms/LoginForm.js
@@ -31,7 +31,8 @@ export default function LoginForm() {
       if (result?.error) {
         setIsError('Please enter correct credentials!');
       } else if (result?.ok) {
-        router.push('/');
+        // await new Promise((resolve) => setTimeout(resolve, 500));
+        router.replace('/');
       }
     } catch (error) {
       console.error('Login Failed', error);
